52d93dc622be968cb7f89cb2b10b4fe98a1c04d2
Hassan-ureshi <hassanqureshi8886@gmail.com>
2026-03-05 13:08:31 -0800

    feat: Add Sika brand with products and images

---
+++ src/data/Sika.ts
@@ -0,0 +1,37 @@
+import sika1 from '../assets/Brands/Sika/sika1.png';
+import sika2 from '../assets/Brands/Sika/sika2.png';
+import sika3 from '../assets/Brands/Sika/sika3.png';
+import sika4 from '../assets/Brands/Sika/sika4.png';
+import sika5 from '../assets/Brands/Sika/sika5.png';
+import sika6 from '../assets/Brands/Sika/sika6.png';
+import sika7 from '../assets/Brands/Sika/sika7.png';
+import sika8 from '../assets/Brands/Sika/sika8.png';
+import sika9 from '../assets/Brands/Sika/sika9.png';
+import sika10 from '../assets/Brands/Sika/sika10.png';
+import sika11 from '../assets/Brands/Sika/sika11.png';
+import sika12 from '../assets/Brands/Sika/sika12.png';
+import sika13 from '../assets/Brands/Sika/sika13.png';
+import sika14 from '../assets/Brands/Sika/sika14.png';
+import sika15 from '../assets/Brands/Sika/sika15.png';
+import sika16 from '../assets/Brands/Sika/sika16.png';
+
+const SikaProducts = [
+  { id: 1, name: "FastFix All Weather", productImg: sika1 },
+  { id: 2, name: "Sika Epoxy Adhesive System", productImg: sika2 },
+  { id: 3, name: "Sika Epoxy Putty Drum Ready – 24 kg – SIKA FLOOR PS", productImg: sika3 },
+  { id: 4, name: "Sika White Sand Bag – SIKA DURE 507", productImg: sika4 },
+  { id: 5, name: "Sika Expansion Joint – White – 600 ml", productImg: sika5 },
+  { id: 6, name: "Sika Expansion Joint – Gray – 600 ml", productImg: sika6 },
+  { id: 7, name: "Sika Latex Bond Drum – 25 Liter", productImg: sika7 },
+  { id: 8, name: "Sika Plug – 108 Bag", productImg: sika8 },
+  { id: 9, name: "Sika Grout Bag – SIKA GROUT 114 SA", productImg: sika9 },
+  { id: 10, name: "Sika Cementitious Waterproofing Material – No. 107", productImg: sika10 },
+  { id: 11, name: "Sika Floor Epoxy Drum – 264 – 7037 – 24 kg", productImg: sika11 },
+  { id: 12, name: "Sika Sand Bag – SIKA DURE 504", productImg: sika12 },
+  { id: 13, name: "Sikafloor Screed 190", productImg: sika13 },
+  { id: 14, name: "Sika Epoxy Polyurethane Drum – Sikafloor 359", productImg: sika14 },
+  { id: 15, name: "Sika Chemical Anchor – SIKA ANCHORFIX-2", productImg: sika15 },
+  { id: 16, name: "Sika Grout Bag – SIKA GROUT 214 SA", productImg: sika16 },
+];
+
+export default SikaProducts;
